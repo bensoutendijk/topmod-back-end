d6c40afdf418298b826dda3f55f6704283221686
Ben Soutendijk <devaent1316@gmail.com>
2019-05-10 00:10:12 -0500

    make changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/auth', require('./authRoutes'));
-router.use('/projects', require('./projectRoutes'));
-router.use('/articles', require('./articleRoutes'));
 
 module.exports = router;
