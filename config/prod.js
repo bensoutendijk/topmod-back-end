8e25a95f7594371b11fe6ebb329663bb22464b5c
Ben Soutendijk <devaent1316@gmail.com>
2019-05-10 20:39:08 -0500

    add config keys to prod file

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,9 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  jwtHttpOnlyKey: process.env.JWT_SECRET_HTTP,
+  jwtKey: process.env.JWT_SECRET,
+  mixerClientId: process.env.MIXER_CLIENT_ID,
+  mixerClientSecret: process.env.MIXER_CLIENT_SECRET,
   mixerCallbackUrl: 'https://bgoodtv.herokuapp.com/api/auth/mixer/callback',
 };
