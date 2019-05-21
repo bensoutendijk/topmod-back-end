764c9a1a55806c12b8e1c6b08ba4602f675928d2
Ben Soutendijk <devaent1316@gmail.com>
2019-05-21 16:59:54 -0500

    added donation route

--- routes/api/notifications/DonationRoutes.js
+++ routes/api/notifications/DonationRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.post('/', (req, res) => {
   console.log(req.body);
-  res.status(200);
+  res.status(200).send();
 });
 
 module.exports = router;
