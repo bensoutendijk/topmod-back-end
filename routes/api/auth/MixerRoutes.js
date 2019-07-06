982007a7eff570b0baf963a0671b9d2970c73ab8
Ben Soutendijk <devaent1316@gmail.com>
2019-07-06 18:59:11 -0500

    reduce mixer oauth scope

--- routes/api/auth/MixerRoutes.js
+++ routes/api/auth/MixerRoutes.js
@@ -10,20 +10,11 @@ router.get('/login',
   passport.authenticate('mixer', {
     scope: [
       'chat:connect',
-      'chat:chat',
-      'chat:change_ban',
-      'chat:change_role',
-      'chat:timeout',
-      'chat:remove_message',
-      'chat:purge',
       'chat:view_deleted',
-      'chat:bypass_links',
-      'chat:bypass_slowchat',
       'channel:analytics:self',
       'channel:details:self',
       'user:details:self',
       'user:analytics:self',
-      'channel:update:self',
     ],
   }));
 
