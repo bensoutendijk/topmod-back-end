08932fb16fc6a9658fbce3d3d2551b563d9e8cf4
Ben Soutendijk <devaent1316@gmail.com>
2019-07-16 19:50:38 -0500

    fix chat route

--- routes/api/chat/MixerRoutes.js
+++ routes/api/chat/MixerRoutes.js
@@ -5,14 +5,13 @@ const auth = require('../../auth');
 const MixerUser = mongoose.model('MixerUser');
 const MixerChatEvent = mongoose.model('MixerChatEvent');
 
-router.get('/:userid', auth.required, async (req, res) => {
-  const { params: { userid }, payload: { _id } } = req;
+router.get('/', auth.required, async (req, res) => {
+  const { payload: { _id } } = req;
 
-  if (userid === _id) {
-    const profile = await MixerUser.findById(userid);
-    const chat = await MixerChatEvent.find({ channel: profile.user.channelid });
-    res.send(chat);
-  }
+  const profile = await MixerUser.findOne({ localUser: _id });
+  const chat = await MixerChatEvent.find({ channel: profile.user.channelid });
+
+  res.send(chat);
 });
 
 module.exports = router;
