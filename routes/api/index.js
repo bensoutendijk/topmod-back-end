7d13b75bd1b1ab4c9a301b19dda3ec6612d277ff
Ben Soutendijk <devaent1316@gmail.com>
2019-07-17 14:46:14 -0500

    remove alerts api

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,5 @@ const router = express.Router();
 router.use('/auth', require('./auth'));
 router.use('/chat', require('./chat'));
 router.use('/analytics', require('./analytics'));
-router.use('/alerts', require('./alerts'));
 
 module.exports = router;
