25061eb18c82644b20eddbdd210ed6304ce7f750
Ben Soutendijk <devaent1316@gmail.com>
2019-07-24 17:36:27 -0500

    add viewership to streams

--- routes/api/analytics/MixerRoutes.js
+++ routes/api/analytics/MixerRoutes.js
@@ -9,15 +9,41 @@ const MixerUser = mongoose.model('MixerUser');
 
 router.get('/streams', auth.required, mixer.auth, async (req, res) => {
   const { mixerUser } = req;
-  const dateFrom = new Date(1970, 1, 1).toISOString();
+  let { query: { to: dateTo, from: dateFrom } } = req;
+
+  if (!dateTo) {
+    dateTo = new Date().toISOString();
+  }
+
+  if (!dateFrom) {
+    dateFrom = new Date(1970, 1, 1).toISOString();
+  }
+
   if (mixerUser) {
-    const URI = `https://mixer.com/api/v1/channels/${mixerUser.user.channelid}/analytics/tsdb/streamSessions?from=${dateFrom}`;
+    const streamsURI = `https://mixer.com/api/v1/channels/${mixerUser.user.channelid}/analytics/tsdb/streamSessions?from=${dateFrom}&to=${dateTo}`;
 
     try {
-      const { data } = await axios.get(URI, {
+      const { data } = await axios.get(streamsURI, {
         headers: { Authorization: `bearer ${mixerUser.tokens.accessToken}` },
       });
-      return res.send(data);
+
+      const streams = await Promise.all(data.map(async (stream) => {
+        const streamStart = new Date(stream.time).toISOString();
+        const streamEnd = new Date(
+          new Date(stream.time).getTime() + stream.duration * 1000,
+        ).toISOString();
+        const viewersURI = `https://mixer.com/api/v1/channels/${mixerUser.user.channelid}/analytics/tsdb/viewers?from=${streamStart}&to=${streamEnd}`;
+
+        const { data: viewership } = await axios.get(viewersURI, {
+          headers: { Authorization: `bearer ${mixerUser.tokens.accessToken}` },
+        });
+
+        Object.assign(stream, { viewership });
+
+        return stream;
+      }));
+
+      return res.send(streams);
     } catch (err) {
       console.log(err);
     }
