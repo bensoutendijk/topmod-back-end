1845ea27c3bc48d4ecf125e92cd54520b169a2fd
Ben Soutendijk <devaent1316@gmail.com>
2019-08-03 22:17:16 -0500

    fix mixer callback url

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -4,4 +4,4 @@ export const jwtHttpOnlyKey = process.env.JWT_SECRET_HTTP;
 export const jwtKey = process.env.JWT_SECRET;
 export const mixerClientId = process.env.MIXER_CLIENT_ID;
 export const mixerClientSecret = process.env.MIXER_CLIENT_SECRET;
-export const mixerCallbackUrl = 'https://topmod.soutendijk.com/api/auth/mixer/callback';
+export const mixerCallbackUrl = 'https://app.topmod.soutendijk.com/api/auth/mixer/callback';
